b9653b527fc7ad92731974fa2455a0ea90a8d001
Blaze Fugina <blaze.fugina@gmail.com>
2018-02-03 15:22:00 -0600

    nav

--- scripts/controllers/nav.controller.js
+++ scripts/controllers/nav.controller.js
@@ -1,5 +1,9 @@
 myApp.controller('NavController', function($interval){
     console.log('Nav Controller created');
     let nc=this;
+
+    function AppCtrl(){
+        nc.currentNavItem = '/';
+    }
     
 });//End Nav Controller
